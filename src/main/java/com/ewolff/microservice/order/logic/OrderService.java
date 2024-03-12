9ab5f70bcd40c294594953d505c69101cbe62d86
Mandy Swinton <mandy.swinton@dynatrace.com>
2024-03-12 14:38:07 -0600

    adding logs- need to be tested

--- src/main/java/com/ewolff/microservice/order/logic/OrderService.java
+++ src/main/java/com/ewolff/microservice/order/logic/OrderService.java
@@ -1,10 +1,16 @@
 package com.ewolff.microservice.order.logic;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ewolff.microservice.order.clients.CatalogClient;
 import com.ewolff.microservice.order.clients.CustomerClient;
+import com.ewolff.microservice.order.clients.Customer;
+
 
 @Service
 class OrderService {
@@ -12,6 +18,8 @@ class OrderService {
 	private OrderRepository orderRepository;
 	private CustomerClient customerClient;
 	private CatalogClient itemClient;
+
+     private static final Logger LOGGER = Logger.getLogger( OrderService.class.getName() );
 
 	@Autowired
 	private OrderService(OrderRepository orderRepository,
@@ -29,6 +37,18 @@ class OrderService {
 		if (!customerClient.isValidCustomerId(order.getCustomerId())) {
 			throw new IllegalArgumentException("Customer does not exist!");
 		}
+        Customer orderCustomer = customerClient.getOne(order.getCustomerId());
+        List<OrderLine> orderLines = order.getOrderLine();
+        int total = 0;
+        for (OrderLine ol: orderLines){
+            total += ol.getCount();
+        }
+
+        StringBuilder logMessage = new StringBuilder();
+        logMessage.append("Customer: ").append(orderCustomer.getName());
+        logMessage.append(" ordered ").append(total);
+        logMessage.append(" items for a total of: $").append(order.totalPrice(itemClient));
+        LOGGER.log(Level.INFO, logMessage.toString());
 		return orderRepository.save(order);
 	}
 
